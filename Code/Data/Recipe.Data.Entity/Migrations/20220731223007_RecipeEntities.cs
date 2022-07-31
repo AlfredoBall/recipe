480aafca400d8be580aac7fc19e41d012e5e7e24
alfredo ball <alfredo.ball@gmail.com>
2022-07-31 17:30:48 -0500

    Recipe Entities Migration

---
+++ Code/Data/Recipe.Data.Entity/Migrations/20220731223007_RecipeEntities.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recipe.Data.Entity.Migrations
+{
+    public partial class RecipeEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Test");
+
+            migrationBuilder.CreateTable(
+                name: "PlanItem",
+                columns: table => new
+                {
+                    PlanItem_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanItem", x => x.PlanItem_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    Recipe_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageData = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.Recipe_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    Ingredient_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.Ingredient_ID);
+                    table.ForeignKey(
+                        name: "FK_Ingredient_Recipe_RecipeID",
+                        column: x => x.RecipeID,
+                        principalTable: "Recipe",
+                        principalColumn: "Recipe_ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instruction",
+                columns: table => new
+                {
+                    Instruction_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instruction", x => x.Instruction_ID);
+                    table.ForeignKey(
+                        name: "FK_Instruction_Recipe_RecipeID",
+                        column: x => x.RecipeID,
+                        principalTable: "Recipe",
+                        principalColumn: "Recipe_ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_RecipeID",
+                table: "Ingredient",
+                column: "RecipeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instruction_RecipeID",
+                table: "Instruction",
+                column: "RecipeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlanItem_Text",
+                table: "PlanItem",
+                column: "Text",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropTable(
+                name: "Instruction");
+
+            migrationBuilder.DropTable(
+                name: "PlanItem");
+
+            migrationBuilder.DropTable(
+                name: "Recipe");
+
+            migrationBuilder.CreateTable(
+                name: "Test",
+                columns: table => new
+                {
+                    Test_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Test", x => x.Test_ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Test_ID",
+                table: "Test",
+                column: "Test_ID",
+                unique: true);
+        }
+    }
+}
